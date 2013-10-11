e9445fb1a7345e20e5a416815ff60f7cb981cb16
Chiedo John <cj@chiedojohn.com>
2013-10-11 17:30:31 -0400

    Fixed jQuery issue

--- app/js/footer.js
+++ app/js/footer.js
@@ -1,41 +1,46 @@
-$("#settings-icon").click(function(){
-  if($(this).hasClass("open")) {
-    $("#settings-panel").animate({ right: "-350px"},500);
-    $("#settings-icon").animate({ right: "10px" },500, function(){
-      $(this).removeClass("open");
+jQuery(document).ready(function() {
+
+  (function($) {
+    $("#settings-icon").click(function(){
+      if($(this).hasClass("open")) {
+        $("#settings-panel").animate({ right: "-350px"},500);
+        $("#settings-icon").animate({ right: "10px" },500, function(){
+          $(this).removeClass("open");
+        });
+      }
+      else {
+        $("#settings-panel").animate({ right: 0},500);
+        $("#settings-icon").animate({ right: $("#settings-panel").width() +10 + "px" },500,function(){
+          $(this).addClass("open");
+        });
+      }
     });
-  }
-  else {
-    $("#settings-panel").animate({ right: 0},500);
-    $("#settings-icon").animate({ right: $("#settings-panel").width() +10 + "px" },500,function(){
-      $(this).addClass("open");
+
+
+    $("#see-disclaimer").click(function(){
+      $("#disclaimer").toggleClass("hidden");
     });
-  }
+
+    $("#settings-update").click(function(){
+      $("#settings-icon").click();
+      wc.changeName();
+      wc.changePhoto();
+    });
+
+    /*
+    IE10 styling
+    */
+    var ie_version = wc.getIEVersion();
+    if (ie_version.major == 10) {
+      $("body").wrapInner("<div class='ie10'>");
+    }
+    wc.autoVertPadding(".vp-auto",".content");
+
+    // First time whisper chirp users
+    if(wc.getCookie("visited_before") != "true") {
+      wc.setCookie("visited_before","true",365);
+      $("#see-disclaimer").click();
+      $("#settings-icon").click();
+    }
+  })( jQuery ); // End scripts
 });
-
-
-$("#see-disclaimer").click(function(){
-  $("#disclaimer").toggleClass("hidden");
-});
-
-$("#settings-update").click(function(){
-  $("#settings-icon").click();
-  wc.changeName();
-  wc.changePhoto();
-});
-
-/*
-IE10 styling
-*/
-var ie_version = wc.getIEVersion();
-if (ie_version.major == 10) {
-  $("body").wrapInner("<div class='ie10'>");
-}
-wc.autoVertPadding(".vp-auto",".content");
-
-// First time whisper chirp users
-if(wc.getCookie("visited_before") != "true") {
-  wc.setCookie("visited_before","true",365);
-  $("#see-disclaimer").click();
-  $("#settings-icon").click();
-}
