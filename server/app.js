75fc315b38e3699c61641eb45b0c6eb6db91c726
Chiedo John <cj@chie.do>
2014-06-17 17:13:36 -0400

    Minor

---
+++ server/app.js
@@ -0,0 +1,5 @@
+var PORT = process.argv[2] && parseInt(process.argv[2], 10) || 5000; 
+var STATIC_DIR = __dirname + '/../app'; 
+var TEST_DIR = __dirname + '/../test'; 
+
+require('./index').start(PORT, STATIC_DIR, TEST_DIR);
